96c66c276437f843984acafaa7007e0a1c691e33
Leigh Halliday <leighhalliday@gmail.com>
2020-12-03 07:43:45 -0500

    Starter

---
+++ pages/index.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+// https://ts.chibicode.com/generics
+// 1) Generics... type parameters
+// 2) "extends" to restrict
+// 3) Default generic types
+// 4) typeof to infer type from variable
+
+function useLocalState(key, initial) {
+  const [value, setValue] = useState(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const saved = window.localStorage.getItem(key);
+      if (saved) {
+        return JSON.parse(saved);
+      }
+    }
+    return initial;
+  });
+
+  useEffect(() => {
+    if (window.localStorage) {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [value]);
+
+  return [value, setValue];
+}
+
+export default function Home() {
+  const [text, setText] = useLocalState("input", "");
+
+  return (
+    <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+  );
+}
